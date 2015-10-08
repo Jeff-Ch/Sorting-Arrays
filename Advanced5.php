3e4aba8c9473c59a1a31b02fd1046a4eacb20b40
Jeff <Jeff@Jeffreys-MacBook-Pro.local>
2015-10-07 18:52:02 -0700

    Initial commit

---
+++ Advanced5.php
@@ -0,0 +1,70 @@
+<?php
+	$start = microtime(true);
+
+	$array = array();
+
+	for($q = 0; $q < 20; $q++){
+		array_push($array, rand(0,20));
+	}
+
+	var_dump($array);
+
+
+// CODE TO SORT ARRAY SELECTING MIN VALUE
+	$count = 0;
+	for($j = 0; $j < count($array); $j++){
+		$min = $array[$j];
+		$max = $array[$j];
+		$minindex = $j;
+		$maxindex = $j;
+		for($i = $j; $i < count($array); $i++){
+			if($array[$i] < $min){
+				$min = $array[$i];
+				$minindex = $i;
+				$count += 1;
+			}
+
+		}
+
+		$place = $array[$j];
+		$array[$j] = $min;
+		$array[$minindex] = $place;
+	}
+
+
+// // CODE TO SORT ARRAY SELECTING BOTH MIN AND MAX VALUES.
+// 	$count = 0;
+// 	for($j = 0; $j < count($array)/2; $j++){
+// 		$min = $array[$j];
+// 		$max = $array[$j];
+// 		$minindex = $j;
+// 		$maxindex = $j;
+// 		for($i = $j; $i < count($array) - $j; $i++){
+// 			if($array[$i] < $min){
+// 				$min = $array[$i];
+// 				$minindex = $i;
+// 				$count += 1;
+// 			}
+// 			if($array[$i] > $max){
+// 				$max = $array[$i];
+// 				$maxindex = $i;
+// 				$count += 1;
+// 			}	
+
+// 		}
+
+// 		$place = $array[$j];
+// 		$array[$j] = $min;
+// 		$array[$minindex] = $place;
+// 		$place2 = $array[(count($array) - 1 - $j)];
+// 		$array[(count($array) - 1 - $j)] = $max;
+// 		$array[$maxindex] = $place2;
+		
+// 	}
+
+	echo 'A total of '.$count.' sort(s) took place resulting in the following sorted array:'.'<br>';
+	var_dump($array);
+	$end = microtime(true);
+	$time = $end - $start;
+	echo 'PHP took '.$time.' second(s) to sort the array!';
+?>
